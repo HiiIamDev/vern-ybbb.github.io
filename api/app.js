02f272ba1955e707ab633bdfee6d15144215e347
HiiIamDev <mribrahim572@gmail.com>
2024-06-22 16:30:45 +0700

    ADDING PENDING STEP VALIDATION, DIFF STYLING. NEXT STYLE THE FORM

--- api/app.js
+++ api/app.js
@@ -7,13 +7,18 @@ import cors from 'cors'
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send('hello world from api :)')
+})
+
 app.post ('/contact', (req, res) => {
     const data = req.body
     if (
         data.email.length <= 7 ||
         !validator.isEmail(data.email) || 
         data.name.length <= 5 ||
-        data.msg.length <= 4
+        data.msg.length <= 4 ||
+        data.name == "ybbbadmin"
     ) {
         return res.status(400).json('Error! something seems not right :( ')
     }
--- src/utils/contacts/contacts.jsx
+++ src/utils/contacts/contacts.jsx
@@ -2,7 +2,7 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet'
 import 'leaflet/dist/leaflet.css'
 import { MapPin } from 'react-feather';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Close } from 'feather-icons-react';
 
 
@@ -20,7 +20,7 @@ function Maps () {
     return (
         <>
         <div className="wrapperMaps">
-            <MapContainer className="container-map" center={position} zoom={17} style={{ height: "400px", width: "100%" }}>
+            <MapContainer center={position} zoom={17} style={{ height: "100%", width: "100%" }}>
                 <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -30,114 +30,216 @@ function Maps () {
                 </Popup>
                 </Marker>
             </MapContainer>
-        </div>
-        <div className="center border-res">
-            <span className='text-location'> <MapPin className='pin'/>Jl. Diponegoro No.22, Kec. Kota Bandung, Jawa Barat 40115 </span>
-        </div>
+            <div className="center border-res">
+                <span className='text-location'> <MapPin className='pin'/>Jl. Diponegoro No.22, Kec. Kota Bandung, Jawa Barat 40115 </span>
+            </div>
+        </div>  
+        
         </>
     )
 }
 
 function Form () {
-    const [form, setForm] = useState({
-        name: "",
-        email: "",
-        msg: ""
-    })
+    // SETTING UP ALL STATES
+    const [ thx, setThx ] =  useState(false) // if all validator state is done, this will be activated to be true for thanks to client ID 1
+    const [ msgLength, setMsgLength ] = useState(false) // message length detecting and setting state to a true ID 2
+    const [ isEmail, setEmail] = useState(false) // state that used to warn if is this an real email or not ID 4
+    const [ isEmpty, setEmpty ] = useState(false) // state that validate to check if an form box is empty ID 5
+    const [ isName, setName ] = useState(false) // state that validate to check if name.length <= 4
+    const [ isError, setIsError ] = useState(false) // state to declared if its something is error on external server
 
-    const [ status, setStatus ] = useState(false)
-    const [ thx, setThx ] =  useState(false)
-    const [ msgLength, setMsgLength ] = useState(false)
+    const nameForm = useRef(null)
+    const emailForm = useRef(null)
+    const msgForm = useRef(null)
 
-    // VALIDATION ABOUT PENDING STATUS
-
-    const [ sendStatus, setSendStatus ] =  useState(true)
-
-    const hitPostContact = async (e) => {
-        // e.preventDefault()
-        const dataContacts = {
-            "name" : form.name,
-            "email" : form.email,
-            "msg" : form.msg
-        }
-
-        try {
-            setSendStatus(false)
-            const response = await fetch('http://localhost:5000/contact', {
-                method: 'POST',
-                headers: {
-                    'Content-Type' : 'application/json',
-                }, body: JSON.stringify(dataContacts)
-            })
-
-            if (response.status == 400) {
-                setStatus(true)
-                setThx(false)
-                setMsgLength(false)
+    const handleRef = (e, nextRef) => {
+        if (e.key === "Enter") {
+            e.preventDefault()
+            if (nextRef === "submit") {
+                handleSubmit(e)
+            } else {
+                nextRef.current.focus()
             }
-
-            console.log('messege sucsessed sending : ', response)
-            setSendStatus(true)
-        
-        } catch (e) {
-            console.log('Error :', e)
-            setSendStatus(true)
         }
     }
- 
-    const formChange = (e) => { 
-        const { name, value } = e.target
-        // console.log(name)
-        setForm({
-            ...form,
-            [name]: value
-        })
-        // console.log(form)
+
+    const altRef = (nextRef) => {
+        switch (nextRef) {
+            case 'name' : 
+                nameForm.current.focus()
+                break
+            case 'email' : 
+                emailForm.current.focus()
+                break
+            case 'msg' : 
+                msgForm.current.focus()
+                break
+        }
     }
+
+    const [ form, setForm ] = useState({
+        "name": "",
+        "email": "",
+        "msg": ""
+    }) // setting up placeholder state data from form
+
+    const [ sendStatus, setSendStatus ] =  useState(true) // if true they'll displayed on conditional operator ID 3
+
+    // CHANGE STATE VALUE INTO DEFAULT VALUE
+
+    function stateDEF () {
+        setThx(false)
+        setMsgLength(false)
+        setEmail(false)
+        setEmpty(false)
+        setName(false)
+        setIsError(false)
+    }
+
+    // filled up data from form 
+    const formChange = (e) => {
+        const { name, value } = e.target // target maybe from onChange parents event
+        setForm({
+            ...form, // ... meaning for looping option on object 
+            [name]: value 
+        })
+    }
+
+    // handling submit
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        if (!(
-            form.name == "" &&
-            form.email == "" &&
-            form.msg == ""
-        )) {
-            // VALIDASI TINGKAT LANJUT
-            if (form.msg.length <= 6) {
-                setMsgLength(true)
-                setThx(false)
-                setStatus(false)
-            } else {
-                sendMsg({
-                    name: form.name,
-                    email: form.email,
-                    msg: form.msg,
-                })
-                setThx(true)
-                setMsgLength(false)
-                // console.log(form)
-                setForm({
-                    name: "",
-                    email: "",
-                    msg: ""
-                })
-            }
-            
+        const isDataValid = dataValidation()
 
-            setStatus(false)
+        if ( isDataValid == 200 ) {
+            sendMsg()
+        } 
+    }
+
+    // validation
+
+    function dataValidation () {
+        let nVer = 0
+        // isEmptyValidation
+        if ((
+            form.name == "",
+            form.email == "",
+            form.msg == ""
+            )) {
+            stateDEF() // change all state value into deffault
+            setEmpty(true)
         } else {
-            setStatus(true)
-            setThx(false)
-            setMsgLength(false)
-            
+            ++nVer
+            // IF NOT EMPTY CALLS MORE VALIDATION 
+
+            // name validation
+
+            if (form.name.length <= 5) {
+                stateDEF()
+                setName(true)
+                altRef('name')
+            } else  {
+                ++nVer
+            } 
+
+            //  email validation
+
+            let gmail = form.email
+
+            if(
+                gmail.includes('@') &&
+                gmail.includes('gmail') &&
+                gmail.includes('.') &&
+                gmail.includes('com')
+            ) {
+                ++nVer
+            } else {
+                stateDEF()
+                setEmail(true)
+                altRef('email')
+            }
+
+            // msg validation 
+
+            if (form.msg.length <= 4) {
+                stateDEF() 
+                setMsgLength(true)
+                altRef('msg')
+            } else {
+                ++nVer
+            }
         }
-        function sendMsg (msg) {
-            hitPostContact()
+
+        // POINT VERIFICATION VALIDATION
+
+        if (nVer == 4) {
+            return 200
+        } else {
+            return 400
         }
     }
 
+    function sendMsg () {
+        let date = new Date()
+        let dataValid = {
+            "name" : form.name,
+            "email" : form.email,
+            "msg" : form.msg,
+            "date" : date.toLocaleString()
+        }
+
+        // console.log('Succsesfull sending: ', dataValid)
+        // PENDING INTO API
+
+        const sending = async (e) => {
+            setSendStatus(false)
+            try {
+                const response = await fetch('http://localhost:5000/contact', {
+                    'method': 'POST',
+                    'headers': {
+                        'Content-type': 'Application/json',
+                    }, 'body': JSON.stringify(dataValid)
+                })
+
+                if (response.status == 200) {
+                    setTimeout(() => {
+                        setSendStatus(true)
+                    }, 1000)
+                    stateDEF()
+                    setThx(true)
+                    setForm({
+                       "name" : "",
+                       "email" : "",
+                       "msg": ""
+                    })
+                }  else {
+                    stateDEF()
+                    setIsError(true)
+                    setTimeout(() => {
+                        setSendStatus(true)
+                    }, 1000)
+                }
+
+            } catch {
+                
+                setTimeout(() => {
+                    stateDEF()
+                    setIsError(true)
+                    setSendStatus(true)
+                }, 500)
+            }
+        }
+
+        sending()
+
+        
+    }
+
     return (
-        <div className="wrapper-form">
+        <>
+            {
+                sendStatus ? (
+            <div className="wrapper-form">
             <form id='contact' className='form-contacts' onSubmit={handleSubmit}>
                 <div className="placeholder">
                     <label htmlFor="name">Name</label>
@@ -147,6 +249,8 @@ function Form () {
                     name="name" 
                     value={form.name} 
                     onChange={formChange}
+                    ref={nameForm}
+                    onKeyPress={(e) => handleRef(e, emailForm)}
                     placeholder='Input Your Name Here'/>
                 </div>
                 <div className="placeholder">
@@ -157,6 +261,8 @@ function Form () {
                     name="email" 
                     value={form.email} 
                     onChange={formChange}
+                    ref={emailForm}
+                    onKeyPress={(e) => handleRef(e, msgForm)}
                     placeholder='Input Your Email Here'/>
                 </div>
                 <div className="placeholder">
@@ -166,53 +272,75 @@ function Form () {
                     name='msg' 
                     value={form.msg} 
                     onChange={formChange}
+                    ref={msgForm}
                     rows={5} 
+                    onKeyPress={(e) => handleRef(e, 'submit')}
                     placeholder='Input Your Message Here'/>
                 </div>
-                <div className="client-output">
+                <div className="client-output">       
                     {
-                        sendStatus ? null : (
-                            <p>loading...</p>
-                        )
-                    }
+                        isError ? (
+                            <>
+                                <p>Something is error in external server, please to try again later :( <button onClick={() => setIsError(false)}>close</button></p>
+                                
+                            </>
+                        ) : null
+                    }                   
                     {
                         msgLength ? (
                             <>
-                              <p>Please Throw Valid Messege <button onClick={() => setMsgLength(false)}>
-                                x</button></p>
+                              <p>Please provide a more specific message <button onClick={() => setMsgLength(false)}>
+                                close</button></p>
                             </>
                         ) : null
                     }
-
-                    {
-                        status ? (
-                            <>
-                            <p>Please Throw A Message <button onClick={
-                            () => {
-                                setStatus(false)
-                            }
-                            }>x </button></p> 
-                            </>
-                        ) 
-                        : null
-                    }
-                    {
+                    {   
                         thx ? (
                             <>
                             <p>Thank you for your feedback :D <button onClick={
                             () => {
                                 setThx(false)
                             }
-                            }>x</button></p> 
+                            }>close</button></p> 
                             </>
                         ) 
                         : null
                     }
+                    {
+                        isEmail ? (
+                            <>
+                                <p>We only accept <a href="https://unacademy.com/content/gate-cse-it/difference-between-email-and-gmail/" target="_blank" title="What's the difference between email and gmail?"><i>Gmail?</i></a> <button onClick={() => setEmail(false)}>close</button></p>
+                                
+                            </>
+                        ) : null
+                    } 
+                    {
+                        isName ? (
+                            <>
+                                <p>We do not accept short names that are less than 5 letters :( <button onClick={() => setName(false)}>close</button></p>
+                                
+                            </>
+                        ) : null
+                    }
+                    {
+                        isEmpty ? (
+                            <>
+                                <p>Please fill in the form <button onClick={() => setEmpty(false)}>close</button></p>
+                                
+                            </>
+                        ) : null
+                    }
+
                 </div>
                 <button type='submit'>Submit</button>
             </form>
-        </div>
-    )
+            </div>
+                ) : (
+                <p>loading...</p>
+            )
+            }
+        </>              
+    ) 
 }
 
 function CardsContacts (
@@ -224,19 +352,24 @@ function CardsContacts (
         <div className="wrapperContacts">
             <Maps />
             {/* DIFFERENT ELEMENT HERE */}
-            <div className="center cardsContacts">
-                <div className=" titleContacts">
-                    <h2>Contact Us</h2>
-                    <span>Jika Anda memiliki pertanyaan atau umpan balik, silakan hubungi kami menggunakan formulir di bawah ini atau melalui informasi kontak di bawah ini</span>                    
-                </div>
-                <div className="wrapper-form">
-                    <div className="opsi-form">
-                        <h3></h3>
+            <div className="cards-flex">
+                <div className="center cardsContacts">
+                    <div className=" titleContacts">
+                        <h2>Contact Us</h2>
+                        {/*<span>Jika Anda memiliki pertanyaan atau umpan balik, 
+                        silakan hubungi kami menggunakan formulir di bawah ini atau melalui informasi kontak 
+                            di bawah ini</span>   */}                
+                        <span className='fs-14'>Jika anda memiliki pertanyaan atau umpan balik, silahkan hubungi kami menggunakan formulir atau melalui beberapa kontak sosial media dibawah ini</span> 
+                    </div>
+                    <div className="wrapper-form">
+                        <div className="opsi-form">
+                            <h3></h3>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="wrapper-side">
-                <Form />
+                <div className="wrapper-side">
+                    <Form />
+                </div>
             </div>
         </div>
     )
